4a89af51e76ce2da34ea2c4f6f0407a36d7ed2f9
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 13:08:19 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,11 +1,3 @@
-"""An example of constructing a profile with a single Xen VM from CloudLab.
-
-Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
-"""
 
 import geni.portal as portal
 import geni.rspec.pg as rspec
@@ -17,14 +9,15 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86.sh"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="bash Anaconda3-5.2.0-Linux-x86.sh -y"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo suwf allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo systemctl status apache2'))
+
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                             command="wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command="bash Anaconda3-5.2.0-Linux-x86_64.sh -b -p /opt/anaconda3"))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command="sudo bash -c echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command="sudo systemctl status apache2"))
 node.addService(rspec.Execute(shell="/bin/sh",
                               command='git clone ttps://github.com/longld/peda.git ~/peda'))
 node.addService(rspec.Execute(shell="/bin/sh",
