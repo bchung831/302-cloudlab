5b52ac8258333690a1f6c593f980a910a56ee293
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 14:58:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ sudo apt-get -y update
 # setup nmap
 sudo apt-get -y install nmap
 
+#Intallation of peda
+sudo git clone https://github.com/longld/peda.git ~/peda
+sudo echo "source ~/peda/peda.py" >> ~/.gbdinit
+
 
 # setup Anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
@@ -21,7 +25,3 @@ sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
 
 # add seed to sudo
 sudo usermod -a -G sudo seed
-
-#Intallation of peda
-sudo git clone https://github.com/longld/peda.git ~/peda
-sudo echo "source ~/peda/peda.py" >> ~/.gbdinit
