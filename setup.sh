0c20e46e78f54aa010aa016f5cbfb1ffc0876ecd
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 14:35:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -x
 
-#
+
 # update Ubuntu's repository
-#
 sudo apt-get -y update
-#
+
+
 # setup nmap
-#
 sudo apt-get -y install nmap
-#
+
+
 # setup Anaconda
-#
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
