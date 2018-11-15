8ef32de91e4ee9a27d26b96b4faf68c3ec9f3d3f
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-15 15:59:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -x
 
-
+#
 # update Ubuntu's repository
+#
 sudo apt-get -y update
 
-
+#
 # setup nmap
+#
 sudo apt-get -y install nmap
 
+#
+# open port 9090 and 9999 for all communications
+#
+sudo ufw allow 9090
+sudo ufw allow 9999
+
+#
 # setup Anaconda
+#
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
