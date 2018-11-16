f8f76eec5af44a2792b2a4bc46abdf16f1edbbe7
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-16 15:26:12 -0500

    Update gdb.sh

--- gdb.sh
+++ gdb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+mkdir peda
+
 # Update gdb to gdb-peda
-git clone https://github.com/longld/peda.git ~/peda
+git clone https://github.com/longld/peda.git peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
