2632b1711bb4808acc4c7fb41d21b807c20a9898
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-16 14:58:56 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,9 +18,13 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
-# Run the bash file to setup Anaconda
+# Run the bash file to set up Anaconda
 node.addService(rspec.Execute(shell="sh", command="sudo chmod 755 /local/repository/setup.sh"))
 node.addService(rspec.Execute(shell="sh", command="sudo /local/repository/setup.sh"))
+
+# Run the bash file to set up gdb-peda
+node.addService(rspec.Execute(shell="sh", command="sudo chmod 755 /local/repository/gdb.sh"))
+node.addService(rspec.Execute(shell="sh", command="sudo /local/repository/gdb.sh"))
 
 #node.addService(rspec.Execute(shell="/bin/sh",
 #                             command='git clone https://github.com/longld/peda.git ~/peda'))
