7aa2040d40558241abe548fd2324d910297cc961
gcardenasfajardo <44878596+gcardenasfajardo@users.noreply.github.com>
2018-11-29 14:32:27 -0500

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,22 @@
+%%writefile tmp/stack.c
+/* Vulnerable program: stack.c */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int bof(char *str)
+{
+  char buffer[24];
+  /* The following statement has a buffer overflow problem */
+  strcpy(buffer, str); //Line 1
+  return 1;
+}
+int main(int argc, char **argv)
+{
+  char str[517];
+  FILE *badfile;
+  badfile = fopen("badfile", "r");
+  fread(str, sizeof(char), 517, badfile);
+  bof(str);
+  printf("Returned Properly\n");
+  return 1;
+}
