1aad78a83ffc8afafa52138ca8b4f14924156442
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-12-04 15:57:57 -0500

    Update setup.sh
    -Fixed issue with anaconda. Seed user is now able to use jupyter notebook
    -Fix issue with 'permission denied' whenever user uses jupyer notebook
    -Fix issue with password for root user.

--- setup.sh
+++ setup.sh
@@ -11,11 +11,6 @@ sudo apt-get -y update
 #
 sudo apt-get -y install nmap
 
-#git clone https://github.com/longld/peda.git ~/peda
-#sudo echo "source ~/peda/peda.py" >> ~/.gdbinit
-#unzip peda
-#sudo unzip -o /local/repository/peda-master.zip -d ~/peda
-
 #
 # setup Anaconda
 #
@@ -24,22 +19,22 @@ sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
 sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
+#setup anaconda for seed
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /home/seed/.bashrc"
+sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /home/seed/.bashrc"
+
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
 
 # create a user named root with password seedubuntu
-sudo useradd -m -p seedubuntu -s /bin/bash root
+sudo usermod root -p xXfH.x2WItKlA
 
-# add seed to sudo
+# add seed to sudo and root
 sudo usermod -a -G sudo seed
+sudo usermod -a -G root seed
 
 #setup gdb peda
 sudo git clone https://github.com/longld/peda.git /home/seed/peda
 sudo chown -R seed:seed /home/seed/peda
 sudo su seed -c "touch /home/seed/.gdbinit"
 sudo echo "source /home/seed/peda/peda.py" >> /home/seed/.gdbinit
-
-#
-# start jupyter
-#
-sudo jupyter notebook --ip * --no-browser 
