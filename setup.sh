af8d1e49dcab15caab6b23e7af81de3c8335ece0
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-12-04 17:47:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,10 +19,6 @@ sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
 sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
-#setup anaconda for seed
-sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /home/seed/.bashrc"
-sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /home/seed/.bashrc"
-
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
 
@@ -38,3 +34,7 @@ sudo git clone https://github.com/longld/peda.git /home/seed/peda
 sudo chown -R seed:seed /home/seed/peda
 sudo su seed -c "touch /home/seed/.gdbinit"
 sudo echo "source /home/seed/peda/peda.py" >> /home/seed/.gdbinit
+
+#setup anaconda for seed
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /home/seed/.bashrc"
+sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /home/seed/.bashrc"
